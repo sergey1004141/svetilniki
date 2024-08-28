c7f317c30a24d9827416b05adde5144711477130
Sergey <sergey10041@gmail.com>
2024-08-28 10:24:31 +0300

    1. вынес маршрут не защещенно, т.к. passport проверяет обратный адрес у клиента

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,10 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::get('/getProduct', [ProductController::class, 'get']);
+//    Route::get('/getProduct', [ProductController::class, 'get']);
 });
 
-//Route::get('/getProduct', [ProductController::class, 'get']);
+Route::get('/getProduct', [ProductController::class, 'get']);
 Route::post('register', [AuthenticationController::class, 'register'])->name('register');
 Route::post('login', [AuthenticationController::class, 'login'])->name('login');
 
